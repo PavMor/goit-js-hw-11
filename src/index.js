01d50043936739ebd0ef8b18e8698d7b0f9d5430
Your Name <pavlo.morozenko@ukr.net>
2023-03-10 19:11:03 +0100

    1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/index.scss';
+import { ImgsApiService } from './js/imgs-service';
+import { cardTemplate } from './js/card-markup.js';
+import { LoadMoreBtn } from './js/load-more-btn';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
