d49742447598be3b491ab2ca6dda599d8c28489d
Your Name <pavlo.morozenko@ukr.net>
2023-03-10 21:19:30 +0100

    4

--- src/index.js
+++ src/index.js
@@ -5,3 +5,83 @@ import { LoadMoreBtn } from './js/load-more-btn';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  imagesContainer: document.querySelector('.gallery'),
+  input: document.querySelector('input[name="searchQuery"]'),
+  searchBtn: document.querySelector('button[type="submit"]'),
+};
+
+const imgsApiService = new ImgsApiService();
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: false,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.input.addEventListener('input', () => (refs.searchBtn.disabled = false));
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
+
+function onSearch(e) {
+  e.preventDefault();
+  /* refs.searchBtn.disabled = true; */
+
+  imgsApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+
+  if (imgsApiService.searchQuery === '') {
+    Notify.failure('Please, enter text');
+    return;
+  }
+
+  loadMoreBtn.show();
+  imgsApiService.resetPage();
+  clearImagesContainer();
+  fetchImages();
+}
+
+function fetchImages() {
+  loadMoreBtn.disable();
+
+  imgsApiService.fetchImages().then(({ data }) => {
+    imgsApiService.totalPage = Math.ceil(data.total / imgsApiService.per_page);
+    imgsApiService.loadedNow += data.hits.length;
+
+    if (imgsApiService.page === 2) {
+      Notify.success(`Hooray! We found ${data.total} images.`);
+    }
+
+    if (imgsApiService.totalPage + 1 === imgsApiService.page) {
+      loadMoreBtn.hide();
+    }
+
+    if (data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    Notify.success(`Loaded ${imgsApiService.loadedNow} images.`);
+    appendImagesMarkup(data.hits);
+    simpleLightbox.refresh();
+
+    loadMoreBtn.enable();
+  });
+}
+
+function appendImagesMarkup(images) {
+  const cardsMarkup = images.map(image => cardTemplate(image));
+  refs.imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup.join(''));
+}
+
+function clearImagesContainer() {
+  refs.imagesContainer.innerHTML = '';
+}
---
+++ src/js/load-more-btn.js
@@ -0,0 +1,30 @@
+export class LoadMoreBtn {
+  constructor({ selector, hidden = true }) {
+    this.refs = this.getRefs(selector);
+
+    hidden && this.hide();
+  }
+
+  getRefs(selector) {
+    const refs = {};
+    refs.button = document.querySelector(selector);
+
+    return refs;
+  }
+
+  enable() {
+    this.refs.button.disabled = false;
+  }
+
+  disable() {
+    this.refs.button.disabled = true;
+  }
+
+  show() {
+    this.refs.button.classList.remove('is-hidden');
+  }
+
+  hide() {
+    this.refs.button.classList.add('is-hidden');
+  }
+}
